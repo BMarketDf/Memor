bc60c431a162b426ab9441f50fe78b9ae259d6ad
BMarketDf <market3business@gmail.com>
2023-06-12 21:35:30 +0100

    add searche

--- lib/page/SearchDelegate.dart
+++ lib/page/SearchDelegate.dart
@@ -1,5 +1,7 @@
 // import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:newproject/cheker_internat_connection.dart';
 // import 'package:newproject/modls/inf_User_M.dart';
 import 'package:newproject/page/sercheSetfulwidget.dart';
 
@@ -8,8 +10,133 @@ import '../modls/srvise/firbase_Servse_to_Hom.dart';
  //هذا الكلاس نتاع سيرش ولفونكسيوا نتاعوا راني دايرهم وحدهم فيكلاس ساتفول ويدجت وستعملة باش نخدم باساتستيت 
 //وهذا هو لكلاس نتاعوا  page/sercheSetfulwidget.dart';
 class Serche extends SearchDelegate{
-  String ?IsSlected="All";
-   String SlectService="تاريخ النشر";
+     List<String> Listselcted=["المستخدمين","خدمات","الكل"];
+    String ?IsSlected="الكل ";
+    HomeModlefirbase serche=HomeModlefirbase();
+    String IsSlectedServis="تاريخ النشر";
+    List<String> Listeserchservice=["تاريخ النشر","الاكثر تقييما","الاكثر تفاعلا",];
+
+   
+Widget getServises(){
+ //بحث ب الخدمات 
+return  Column(
+  children: [
+   SlectTypeService(),   
+     FutureBuilder(
+    
+        future:serche.getSearche(query,IsSlectedServis),
+    
+          builder: (context, snapshot) {
+    
+              if(!snapshot.hasData){
+    
+                return const Center(child:CircularProgressIndicator() ,);
+    
+              }else{
+    
+                if (snapshot.data!.isEmpty) {
+    
+                   return Center(child:Text("لايوجد خدمات باسم  $query"));
+                }    
+                 return  Expanded(    
+                   child: ListView.builder(
+    
+                    itemCount: snapshot.data!.length,
+    
+                    // scrollDirection: Axis.horizontal,
+    
+                    itemBuilder: (context, index){
+    
+                         return InkWell(
+    
+                          onTap: (){
+    
+                          },
+    
+                       child: Padding(
+    
+      padding: const EdgeInsets.symmetric(horizontal: 10.0,vertical: 10.0*0.75),
+    
+       child: Column(children:[
+    
+        CachedNetworkImage(
+    
+         imageUrl: snapshot.data![index].imgUrl.toString(),
+    
+         errorWidget: (context, url, error) => const Icon(Icons.person,color: Colors.blue,),
+    
+         placeholder:(context,url)=> const Icon(Icons.person,color:Colors.blue),
+    
+           imageBuilder: (context, imageProvider) =>
+    
+         
+    
+          Image(image: imageProvider,
+    
+           fit: BoxFit.scaleDown, ),
+    
+      ),
+    
+        const SizedBox(width: 25,),
+    
+          Text("${snapshot.data![index].title}",style: const TextStyle(fontSize:17,
+    
+       fontWeight:FontWeight.w600),),
+    
+       Text("Raitin ${snapshot.data[index].Raitin}"),
+    
+       Text("Count ${snapshot.data[index].Count}"),
+    
+       Text("date ${snapshot.data[index].createDate}")
+
+       ],),),);},),
+    
+                 );} },),
+  ],
+); 
+  }
+
+    Widget getUsers(){
+ //بحث ب الخدمات 
+return  FutureBuilder(
+    future:serche.getSearcheUser(query),
+      builder: (context, snapshot) {
+          if(!snapshot.hasData){
+            return const Center(child:CircularProgressIndicator() ,);
+          }else{
+            if (snapshot.data!.isEmpty) {
+               return Center(child:Text("لايوجدمستخدمين باسم  $query"));
+            }
+             return  Expanded(
+               child: ListView.builder(
+                itemCount: snapshot.data!.length,
+                scrollDirection: (IsSlected=="المستخدمين")? Axis.vertical:Axis.horizontal,
+                itemBuilder: (context, index){
+                     return Container(
+                      height: 1,
+                       child: InkWell(
+                        onTap: (){
+                        },
+                         child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10.0,vertical: 10.0*0.75),
+                           child: Row(children:[
+                            CircleAvatar(
+                              radius: 25,
+                              child: CachedNetworkImage(
+                               imageUrl: snapshot.data![index].imgeurl.toString(),
+                               errorWidget: (context, url, error) => const Icon(Icons.person,color: Colors.blue,),
+                               placeholder:(context,url)=> const Icon(Icons.person,color:Colors.blue),
+                                 imageBuilder: (context, imageProvider) =>
+                                Image(image: imageProvider,
+                                 fit: BoxFit.scaleDown, ),
+                                                      ),),
+                              const SizedBox(width: 20,),
+                            Text("${snapshot.data![index].Username}",style:const TextStyle(fontWeight: FontWeight.bold)),
+                           ],),),),
+                     );},),
+             );} },); 
+  }
+
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [IconButton(onPressed: (){
@@ -23,11 +150,70 @@ class Serche extends SearchDelegate{
         }, icon:const  Icon(Icons.arrow_back));}
     @override
    Widget buildResults(BuildContext context) {
-    return serchedelog(Typeserche: false,SlectService:SlectService,query:query ,IsSlected: IsSlected,);
+      if(query.isEmpty){
+        return const Center(child: Text("الرجاء ادخل قيمةgg البحث  "),);
+      }else{
+         if (IsSlected=="المستخدمين") {
+             return getUsers(); 
+         } else {         if (IsSlected=="خدمات") {
+                   return  getServises();
+         }else{
+
+          return ListView(children: [
+                  getServises(),
+                  const SizedBox(height: 150,),
+                   SizedBox(
+                    height: 550,
+                    child: getUsers()),
+           ],);
          }
+         }
+      }
+        }
   @override
   Widget buildSuggestions(BuildContext context) {
-    return serchedelog(IsSlected:IsSlected,Typeserche: true,);
-    // Home(IsSlected: IsSlected,
-    // );
-  }}
+    return Container(
+      height: 40,
+      child: ListView.builder(
+        itemCount:Listselcted.length ,
+        scrollDirection: Axis.horizontal,
+        itemBuilder:(context, index){   
+          return SizedBox(
+            // height: 200,
+            width: 200,
+            child: Padding(
+  padding: const EdgeInsets.only(left: 7.0,top: 7.0),
+  child: ElevatedButton(onPressed: (){
+   IsSlected=Listselcted[index];
+  }, child:  Center(child: Text(Listselcted[index]),),
+  //  style:  ButtonStyle(),
+  ),
+),);}  ),   );
+  }
+Widget SlectTypeService(){
+
+return SizedBox(
+  height: 40,
+child: ListView.builder(
+  itemCount: Listeserchservice.length,
+  scrollDirection: Axis.horizontal,
+  itemBuilder: 
+(context, index) {
+  return Container(
+ height:200,
+ width:150,
+ child:
+   Padding(
+     padding: const EdgeInsets.only(left: 7.0,top: 7.0),
+     child: ElevatedButton(onPressed: (){
+      IsSlectedServis=Listeserchservice[index];
+     }, child:  Center(child: Text(Listeserchservice[index]),),
+     //  style:  ButtonStyle(),
+     ),
+   ),
+      
+   );
+})
+   );
+     }
+  }
--- lib/page/sercheSetfulwidget.dart
+++ lib/page/sercheSetfulwidget.dart
@@ -1,157 +1,177 @@
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
+// // ignore_for_file: void_checks
 
-import '../modls/srvise/firbase_Servse_to_Hom.dart';
+// import 'package:cached_network_image/cached_network_image.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/foundation.dart';
 
-class serchedelog extends StatefulWidget {
-        String ?IsSlected;
-        String ? SlectService;
-        String ? query;
-        bool Typeserche;
-   serchedelog({super.key,this.SlectService,this.IsSlected,required this.Typeserche,this.query,});
-  @override
-  State<serchedelog> createState() => _Serche();
-}
- class _Serche extends State<serchedelog> {
- HomeModlefirbase serche=HomeModlefirbase();
-  List<String> Listselcted=["Users","Service","All"];
-  List<String> Tupeserchservice=["تاريخ النشر","الاكثر تقييما","الاكثر تفاعلا"];
-  //fonction get slected 
-  Widget getslected(){
-    //هذه الدالة نتاع يخير وش حاب يريشارشي نتاع سرفيس ولا المستخدم ومزال نقصتها خدما 
-     return ListView.builder(
-       itemCount: Listselcted.length,
-      scrollDirection: Axis.horizontal,
-      itemBuilder:(context, index) { 
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children:[ 
-           Row(children:[Text("${Listselcted[index]}"),
-          Radio(value:Listselcted[index], groupValue:widget.IsSlected , onChanged: (val){
-            setState(() {
-               widget.IsSlected=val!;
-            });})],), ]);});
-  }
+// import '../modls/srvise/firbase_Servse_to_Hom.dart';
 
-
-  Widget getUsers(){
-   //بحث ب الخدمات 
-  return  FutureBuilder(
-      future:serche.getSearcheUser(widget.query!),
-        builder: (context, snapshot) {
-            if(!snapshot.hasData){
-              return const Center(child:CircularProgressIndicator() ,);
-            }else{
-              if (snapshot.data!.isEmpty) {
-                 return Center(child: Text("لاتوجد نتائج ب ${widget.query}"));
-              }
-               return  Expanded(
-                 child: ListView.builder(
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (context, index){
-                       return InkWell(
-                        onTap: (){
-                        },
-                         child: Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 10.0,vertical: 10.0*0.75),
-                           child: Row(children:[
-                            Text("${snapshot.data![index].Username}"),
-                            CircleAvatar(
-                              radius: 25,
-                              child: CachedNetworkImage(
-                               imageUrl: snapshot.data![index].imgUrl.toString(),
-                               errorWidget: (context, url, error) => const Icon(Icons.person,color: Colors.blue,),
-                               placeholder:(context,url)=> const Icon(Icons.person,color:Colors.blue),
-                                 imageBuilder: (context, imageProvider) =>
-                                Image(image: imageProvider,
-                                 fit: BoxFit.scaleDown, ),
-                                                      ),
-                            ),
-                            const SizedBox(width: 25,),
-                              Text("${snapshot.data![index].title}",style: const TextStyle(fontSize:17,
-                           fontWeight:FontWeight.w600),),                           
-                           ],),),);},),
-               );} },); 
-    }
-
-
-  
-   Widget getservis(){
-    //بحث ب الخدمات 
-   return  Column(
-     children: [
-      Expanded(
-        child: ListView.builder(
-         itemCount: Tupeserchservice.length,
-        scrollDirection: Axis.horizontal,
-        itemBuilder:(context, index) { 
-        return Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Row(children: [Text("${Tupeserchservice[index]}"),
-              Radio(value:Tupeserchservice[index], groupValue:widget.SlectService , onChanged: (val){
-                setState(() {
-                  
-                   widget.SlectService=val!;
-                   print(widget.SlectService);
-                   print(val);
-                }); 
-                }) ], ),
-          ],
-        ); }),
-      ),
-    FutureBuilder(
-         future:serche.getSearche(widget.query!,widget.SlectService!),
-          builder: (context, snapshot) {
-              if(!snapshot.hasData){
-                return const Center(child:CircularProgressIndicator(),);
-              }else{
-                if (snapshot.data!.isEmpty) {
-                   return Center(child: Text("لاتوجد نتائج ب ${widget.query}"));
-                }
-                 return  Expanded(
-                   child: ListView.builder(
-                    itemCount: snapshot.data!.length,
-                    itemBuilder: (context, index){
-                         return InkWell(
-                          onTap: (){
-                          },
-                           child: Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 10.0,vertical: 10.0*0.75),
-                             child: Column(children:[
-                              CachedNetworkImage(
-                               imageUrl: snapshot.data![index].imgUrl.toString(),
-                               errorWidget: (context, url, error) => const Icon(Icons.person,color: Colors.blue,),
-                               placeholder:(context,url)=> const Icon(Icons.person,color:Colors.blue),
-                                 imageBuilder: (context, imageProvider) =>
+// class serchedelog extends StatefulWidget {
+//         String ? SlectService;
+//         String ? query;
+//         bool Typeserche;
+//    serchedelog({super.key,this.SlectService,required this.Typeserche,this.query,});
+//   @override
+//   State<serchedelog> createState() => _Serche();
+// }
+//  class _Serche extends State<serchedelog> {
+//  HomeModlefirbase serche=HomeModlefirbase();
+//       String  IsSlected="All";
+//       String IsSlectedServis="تاريخ النشر";
+//   List<String> Listselcted=["Users","Service","All"];
+//   List<String> Listeserchservice=["تاريخ النشر","الاكثر تقييما","الاكثر تفاعلا"];
+//   //fonction get slected 
+//    propse(String ?slected){ 
+//     //هذه الدالة نتاع يخير وش حاب يريشارشي نتاع سرفيس ولا المستخدم ومزال نقصتها خدما 
+//      return ListView.builder(
+//        itemCount: Listselcted.length,
+//       scrollDirection: Axis.horizontal,
+//       itemBuilder:(context, index) { 
+//       return SizedBox(
+//        width: 200,
+//        height: 200,
+//         child: RadioListTile(
+//            title:Text(Listselcted[index]),
+//           value:Listselcted[index], groupValue:IsSlected ,
+//           onChanged: (val){
+//           setState(() {
+//             IsSlected=val!;
+//             slected=Listselcted[index];
+//             print(Listselcted[index]);
+//             print("//////////============/////////////");
+//              print(IsSlected);
+//          });
+//          }),
+//       );});
+       
+//   }
+//   // getslected(){
+// //  switch (widget.SlectService) {
+//     //  case "مستخدمين":
+//   //  getUsers();
+//   //  break;
+// //  case "الكل":
+//   // getUsers();
+// //  getservis();
+// //  break;
+// // default:
+// // getservis();
+// //  }
+//   // }
+//   Widget getUsers(){
+//    //بحث ب الخدمات 
+//   return  FutureBuilder(
+//       future:serche.getSearcheUser(widget.query!),
+//         builder: (context, snapshot) {
+//             if(!snapshot.hasData){
+//               return const Center(child:CircularProgressIndicator() ,);
+//             }else{
+//               if (snapshot.data!.isEmpty) {
+//                  return Center(child:Text("لايوجدمستخدمين باسم  ${widget.query}"));
+//               }
+//                return  Expanded(
+//                  child: ListView.builder(
+//                   itemCount: snapshot.data!.length,
+//                   scrollDirection: Axis.horizontal,
+//                   itemBuilder: (context, index){
+//                        return InkWell(
+//                         onTap: (){
+//                         },
+//                          child: Padding(
+//                             padding: const EdgeInsets.symmetric(horizontal: 10.0,vertical: 10.0*0.75),
+//                            child: Row(children:[
+//                             CircleAvatar(
+//                               radius: 25,
+//                               child: CachedNetworkImage(
+//                                imageUrl: snapshot.data![index].imgeurl.toString(),
+//                                errorWidget: (context, url, error) => const Icon(Icons.person,color: Colors.blue,),
+//                                placeholder:(context,url)=> const Icon(Icons.person,color:Colors.blue),
+//                                  imageBuilder: (context, imageProvider) =>
+//                                 Image(image: imageProvider,
+//                                  fit: BoxFit.scaleDown, ),
+//                                                       ),),
+//                               const SizedBox(width: 20,),
+//                             Text("${snapshot.data![index].Username}",style:const TextStyle(fontWeight: FontWeight.bold)),
+//                            ],),),);},),
+//                );} },); 
+//     }
+//    Widget getservis(){
+//     //بحث ب الخدمات 
+//    return  Column(
+//      children: [
+//       Expanded(
+//         child: SizedBox(
+//           child: ListView.builder(
+//            itemCount: Listeserchservice.length,
+//           scrollDirection: Axis.horizontal,
+//           itemBuilder:(context, index) { 
+//           return Column(
+//             mainAxisAlignment: MainAxisAlignment.start,
+//             children: [
+//               Row(children: [Text(Listeserchservice[index]),
+//                 Radio(value:Listeserchservice[index], groupValue:IsSlectedServis , onChanged: (val){
+//                   setState(() {       
+//                      IsSlectedServis=val!; }); })],),],);}),
+//         ),),
+//         FutureBuilder(
+//          future:serche.getSearche(widget.query!,IsSlectedServis),
+//           builder: (context, snapshot) {
+//               if(!snapshot.hasData){
+//                 return const Center(child:CircularProgressIndicator(),);
+//               }else{
+//                 if (snapshot.data!.isEmpty) {
+//                    return Center(child: Text("لايوجد خدمات  ${widget.query}"));
+//                 }
+//                  return  Expanded(
+//                    child: ListView.builder(
+//                     itemCount: snapshot.data!.length,
+//                     itemBuilder: (context, index){
+//                          return InkWell(
+//                           onTap: (){
+//                           },
+//                            child: Padding(
+//                             padding: const EdgeInsets.symmetric(horizontal: 10.0,vertical: 10.0*0.75),
+//                              child: Column(children:[
+//                               CachedNetworkImage(
+//                                imageUrl: snapshot.data![index].imgUrl.toString(),
+//                                errorWidget: (context, url, error) => const Icon(Icons.person,color: Colors.blue,),
+//                                placeholder:(context,url)=> const Icon(Icons.person,color:Colors.blue),
+//                                  imageBuilder: (context, imageProvider) =>
                                
-                                Image(image: imageProvider,
-                                 fit: BoxFit.scaleDown, ),
-                            ),
-                              const SizedBox(width: 25,),
-                                Text("${snapshot.data![index].title}",style: const TextStyle(fontSize:17,
-                             fontWeight:FontWeight.w600),),
-                             Text("Raitin ${snapshot.data[index].Raitin}"),
-                             Text("Count ${snapshot.data[index].Count}"),
-                             Text("date ${snapshot.data[index].createDate}")
+//                                 Image(image: imageProvider,
+//                                  fit: BoxFit.scaleDown, ),
+//                             ),
+//                               const SizedBox(width: 25,),
+//                                 Text("${snapshot.data![index].title}",style: const TextStyle(fontSize:17,
+//                              fontWeight:FontWeight.w600),),
+//                              Text("Raitin ${snapshot.data[index].Raitin}"),
+//                              Text("Count ${snapshot.data[index].Count}"),
+//                              Text("date ${snapshot.data[index].createDate}")
                              
-                             ],),),);},),
-                 );} },), 
-    ],); 
-     }
-  @override
-  Widget build(BuildContext context) {
-    if (widget.Typeserche) 
-    {//momken ytbdel hda vareaible youli int 
-      return getslected();
-    }else{
-      if(widget.IsSlected=="Users"){
-      return getUsers();
-      }else{
-      return getservis();
-      }
-    }
-     }
-}
+//                              ],),),);},),
+//                  );} },), 
+//     ],); 
+//      }
+//   @override
+//   Widget build(BuildContext context) {
+//     if (widget.Typeserche) 
+//     {//momken ytbdel hda vareaible youli int 
+//      print("=================================================");
+//  print(IsSlected);
+//  print("===============================================================");
+//        return propse(IsSlected);
+//     }
+//      else{
+//       if(IsSlected=="Users"){
+//          return getUsers();
+//       }
+//       else{
+       
+       
+       
+//       return getservis();
+//       }
+//      }
+//      }
+// }
